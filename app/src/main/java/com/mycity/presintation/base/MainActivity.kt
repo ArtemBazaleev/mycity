46a371aea45ea71e0f656131250d92999bb606dd
ArtemBazaleev <a.bazaleev@gmail.com>
2021-03-13 15:33:04 +0300

    inited maps

--- app/src/main/java/com/mycity/presintation/base/MainActivity.kt
+++ app/src/main/java/com/mycity/presintation/base/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.FrameLayout
 import com.mycity.R
 import com.mycity.presintation.module.map.ui.fragment.GoogleMapFragment
+import com.mycity.presintation.module.map.ui.fragment.MapsFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initFragment() {
-        val fragment = GoogleMapFragment()
+        val fragment = MapsFragment()
         val fm = supportFragmentManager
         fm.beginTransaction()
             .add(R.id.fl_root,fragment)
